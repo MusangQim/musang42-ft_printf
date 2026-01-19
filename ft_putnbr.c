7223d2382c3ebcb3d07e7825612bef9fdf5ec92b
MusangQim <musangqim03@gmail.com>
2026-01-19 18:16:06 +0800

    update file

--- ft_putnbr.c
+++ ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/19 13:48:49 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/01/19 13:49:12 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/01/19 18:15:52 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,4 +14,18 @@
 
 int	ft_putnbr(int n)
 {
+	long		number;
+	int			len;
+
+	number = n;
+	len = 0;
+	if (number < 0)
+	{
+		len += write(1, "-", 1);
+		number = -number;
+	}
+	if (number >= 10)
+		len += ft_putnbr(number / 10);
+	len += ft_putnbr((number % 10) + '0');
+	return (len);
 }
