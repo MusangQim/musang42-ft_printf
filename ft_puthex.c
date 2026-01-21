1c9e1a945f8dde61f90911e2cfbb619c6a274f98
MusangQim <musangqim03@gmail.com>
2026-01-21 22:46:06 +0800

    update file

--- ft_puthex.c
+++ ft_puthex.c
@@ -6,7 +6,7 @@
 /*   By: adzmusta <adzmusta@student.42iskandarpute  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/21 22:42:57 by adzmusta          #+#    #+#             */
-/*   Updated: 2026/01/21 22:44:43 by adzmusta         ###   ########.fr       */
+/*   Updated: 2026/01/21 22:45:55 by adzmusta         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,4 +22,8 @@ int	ft_puthex(unsigned int n, char format)
 		base = "0123456789abcdef";
 	else
 		base = "0123456789ABCDEF";
+	if (n >= 16)
+		len += ft_puthex(n / 16, format);
+	len += ft_putchar(base[n % 16]);
+	return (len);
 }
